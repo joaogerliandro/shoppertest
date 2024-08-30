c597ad71d082acf307f52bf4d4537aa7f512cd43
João Gerliandro <joaogerliandro@gmail.com>
2024-08-30 14:52:46 -0300

    feat: adding to upload endpoint better request validations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { z } from 'zod';
+import { parse, isValid } from 'date-fns';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -17,11 +18,50 @@ app.listen(port, () => {
   console.log(`[SERVER]: Server is running at PORT: ${port}`);
 });
 
+const isBase64Image = (image: string): boolean => {
+    const base64ImageRegex = /^data:image\/(png|jpg|jpeg|gif|bmp|webp);base64,/;
+    return base64ImageRegex.test(image);
+};
+
+const isDateTimeValid = (date_time: string): boolean => {
+    const formats = [
+        "yyyy-MM-dd'T'HH:mm:ss.SSSxxx", 
+        'dd/MM/yyyy', 
+        'MM/dd/yyyy', 
+        "yyyy/MM/dd", 
+        'dd-MM-yyyy', 
+        'MM-dd-yyyy', 
+        'yyyy-MM-dd', 
+        'yyyy.MM.dd', 
+        'dd MMM yyyy', 
+        'yyyy/MM/dd HH:mm:ss', 
+        'MM/dd/yyyy HH:mm:ss', 
+        'yyyy-MM-ddTHH:mm',
+    ];
+    
+    return formats.some(format => {
+        const parsedDate = parse(date_time, format, new Date());
+        return isValid(parsedDate);
+    });
+};
+
 const measurementSchema = z.object({
-    image: z.string(),
-    customer_code: z.string(),
-    measure_datetime: z.string().refine((val) => !isNaN(Date.parse(val)), {
-      message: "Invalid date format",
+    image: z.string()
+    .min(1, {
+        message: "Image Base64 can not be empty !"
+    })
+    .refine(val => isBase64Image(val), {
+        message: "Invalid Base64 image format"
+    }),
+    customer_code: z.string().min(1, {
+        message: "Customer Code can not be empty !",
+    }),
+    measure_datetime: z.string()
+    .min(1, {
+        message: "Datetime can not be empty !",
+    })
+    .refine((val) => isDateTimeValid(val), {
+        message: "Invalid datetime format",
     }),
     measure_type: z.enum(['WATER', 'GAS'])
 });
